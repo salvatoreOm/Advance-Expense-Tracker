143612b3c2adba0026500e2ac89ffcb611fac106
OM PARIHAR <omparihar0001@gmail.com>
2025-01-29 13:22:42 +0530

    create admin user

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,6 @@ class Expense(models.Model):
     amount = models.IntegerField()
     category = models.CharField(max_length=50)
     date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
